50177424524a4647e0dfd0d69186679082ab444f
Gerhard de Clercq <gerharddeclercq@outlook.com>
2016-06-17 16:09:43 +0200

    Satrted implementing triangle slicing.

--- ChopperEngine/chopperengine.cpp
+++ ChopperEngine/chopperengine.cpp
@@ -1,10 +1,17 @@
 #include "chopperengine.h"
 #include "Misc/globalsettings.h"
+#include "clipper.hpp"
 #include <iostream>
+
+using namespace ChopperEngine;
+using namespace ClipperLib;
 
 std::size_t ChopperEngine::layerCount = 0;
 Mesh* ChopperEngine::sliceMesh = nullptr;
-static ChopperEngine::LogDelegate slicerLogger = nullptr;
+static LogDelegate slicerLogger = nullptr;
+
+// Scale double to ints with this factor
+static double scaleFactor = 1000000.0;
 
 void ChopperEngine::SlicerLog(std::string message)
 {
@@ -14,17 +21,136 @@ void ChopperEngine::SlicerLog(std::string message)
     std::cout << message << std::endl;
 }
 
+static inline void getTrigPointFloats(Triangle &trig, double *arr, uint8_t pnt)
+{
+    arr[0] = (double)sliceMesh->vertexFloats[trig.vertIdxs[0] * 3 + pnt];
+    arr[1] = (double)sliceMesh->vertexFloats[trig.vertIdxs[1] * 3 + pnt];
+    arr[2] = (double)sliceMesh->vertexFloats[trig.vertIdxs[2] * 3 + pnt];
+}
+
+static void SliceTrigsToLayers()
+{
+    ChopperEngine::SlicerLog("Slicing triangles into layers");
+
+    for (std::size_t i = 0; i < layerCount; i++)
+    {
+        double zPoint = (double)i * GlobalSettings::LayerHeight.Get();
+
+        for (std::size_t j = 0; j < sliceMesh->trigCount; j++)
+        {
+            Triangle &trig = sliceMesh->trigs[j];
+
+            // Check if the triangle contains the current layer's z
+            double z[3];
+            getTrigPointFloats(trig, z, 2);
+            double minZ = std::min(z[0], std::min(z[1], z[2]));
+            double maxZ = std::max(z[0], std::max(z[1], z[2]));
+            if (zPoint <= maxZ && zPoint >= minZ)
+            {
+                // Determine at which points the z is on the triangle
+
+                // We need to determine which two sides of the triangle intersect with the z point
+                // point A should be the point where these two sides connect
+                // point B and C should therefore be the other two points
+                uint8_t a, b, c;
+                bool set = false;
+                if (zPoint == z[0])
+                {
+                    if (zPoint == z[1])
+                    {
+                        a = 2;
+                        b = 0;
+                        c = 1;
+                        set = true;
+                    }
+                    else if (zPoint == z[3])
+                    {
+                        a = 1;
+                        b = 2;
+                        c = 0;
+                        set = true;
+                    }
+                }
+                else if (zPoint == z[1] && zPoint == z[2])
+                {
+                    a = 0;
+                    b = 1;
+                    c = 2;
+                    set = true;
+                }
+
+                if (!set)
+                {
+                    bool oneTwo = false;
+                    bool oneThree = false;
+                    bool twoThree = false;
+
+                    if ((zPoint <= z[0] && zPoint >= z[1]) || (zPoint >= z[0] && zPoint <= z[1]))
+                        oneTwo = true;
+                    if ((zPoint <= z[0] && zPoint >= z[2]) || (zPoint >= z[0] && zPoint <= z[2]))
+                        oneTwo = true;
+                    if ((zPoint <= z[1] && zPoint >= z[2]) || (zPoint >= z[1] && zPoint <= z[3]))
+                        oneTwo = true;
+
+                    if (oneTwo && oneThree)
+                    {
+                        a = 0;
+                        b = 1;
+                        c = 2;
+                    }
+                    else if (oneTwo && twoThree)
+                    {
+                        a = 1;
+                        b = 2;
+                        c = 0;
+                    }
+                    else
+                    {
+                        a = 2;
+                        b = 0;
+                        c = 1;
+                    }
+                }
+
+                double x[3], y[3];
+                getTrigPointFloats(trig, x, 0);
+                getTrigPointFloats(trig, y, 0);
+
+                // First calculate the relationship of z to x on the one side of the triangle
+                double zToX1 = (z[a] != z[b]) ? (x[a] - x[b] / z[a] - z[b]) : 0;
+                // Then calculate the relationship of z to y on the one side of the triangle
+                double zToY1 = (z[a]  != z[b]) ? (y[a] - y[b] / z[a] - z[b]) : 0;
+                // Then calculate the relationship of z to x on the other side of the triangle
+                double zToX2 = (z[a]  != z[c]) ? (x[a] - x[c] / z[a] - z[c]) : 0;
+                // Then calculate the relationship of z to y on the other side of the triangle
+                double zToY2 = (z[a]  != z[c]) ? (y[a] - y[c] / z[a] - z[c]) : 0;
+
+                // Now calculate the z rise above pointB
+                double zRise1 = zPoint - z[b];
+                // And also the z rise above pointC
+                double zRise2 = zPoint - z[c];
+
+                // We can now calculatet the x and y points on both sides of the triangle using the z rises that were calculated above
+                IntPoint p1 = IntPoint((cInt)(x[b] + zToX1 * zRise1 * scaleFactor),
+                                       (cInt)(y[b] + zToY1 * zRise1 * scaleFactor));
+                IntPoint p2 = IntPoint((cInt)(x[c] + zToX2 * zRise2 * scaleFactor),
+                                       (cInt)(y[c] + zToY2 * zRise2 * scaleFactor));
+            }
+        }
+    }
+}
+
 void ChopperEngine::SliceFile(Mesh *inputMesh, std::string outputFile)
 {
     sliceMesh = inputMesh;
 
     // Calculate the amount layers that will be sliced
-    layerCount = sliceMesh->MaxVec.z / GlobalSettings::LayerHeight;
+    layerCount = (std::size_t)(sliceMesh->MaxVec.z / GlobalSettings::LayerHeight.Get());
 
     // Slice the triangles into layers
+    SliceTrigsToLayers();
 
     // Calculate islands from the original lines
-
     // Optimize the outline polygons
 
     // Generate the outline segments
